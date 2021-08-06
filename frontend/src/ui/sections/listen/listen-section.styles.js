df7cd5a9c4870e82796afa04c39cecb607ed324a
Luciano <luko.web@gmail.com>
2021-08-06 19:39:43 -0300

    Invisible container again

--- frontend/src/ui/sections/listen/listen-section.styles.js
+++ frontend/src/ui/sections/listen/listen-section.styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 width: 100%;
-margin-bottom: 20px;
 background-color: red;
 `
 
--- frontend/src/ui/sections/listen/play-song.styles.js
+++ frontend/src/ui/sections/listen/play-song.styles.js
@@ -2,67 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 width: 100%;
-height: 200px;
 `
 
 export const InvisibleContainer = styled.div`
 width:100%;
-height: 0px;
+height: 1px;
 visibility: hidden;
 `
-
-// /* youtube */
-// .PlaySong_container {
-// 	width: 100%;
-//   }
-
-//   .Youtube_invisible_container {
-//     visibility: hidden;
-//   }
-
-//   .PlaySong_play_the_song_video {
-
-//   }
-
-//   .PlaySong_button_timer_container {
-//     width: 10%;
-//     height: 200px;
-//     background-color: black;
-//     display: inline;
-//     float: left;
-//   }
-
-//   .PlaySong_button_container {
-//     width: 100px;
-//     height: 100px;
-//     display: inline;
-//     float: left;
-//   }
-
-//   .PlaySong_button {
-//     width: 0;
-//     height: 0;
-//     border-top: 20px solid black;
-//     border-left: 35px solid white;
-//     border-bottom: 20px solid black;
-//     margin: 0 auto;
-//     margin-left: 25px;
-//     margin-top: 25px;
-//     cursor: pointer;
-//   }
-
-//   .PlaySong_timer_container {
-//     background-color: blue;
-//     display: inline;
-//     float: left;
-//     width: 100px;
-//     height: 100px;
-//   }
-
-//   .PlaySong_timer {
-//     width: 50px;
-//     height: 40px;
-//     margin: 0 auto;
-//     background-color: black;
-//   }
-
