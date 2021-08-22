f160359bea4c9d22fb6cabd7956a4db7f45aaeee
Luciano <luko.web@gmail.com>
2021-08-21 22:38:23 -0300

    Cleaned up 'analysis component' file

--- frontend/src/ui/pages/analysis/analysis.component.jsx
+++ frontend/src/ui/pages/analysis/analysis.component.jsx
@@ -17,31 +17,19 @@ class AnalysisPage extends React.Component {
    constructor(props) {
 		super(props);
 		this.state = { 
-			// Loading...
+	
 			loading: true, // indica que la página se está cargando aún
 			
 			// Song Timer
 			currentTimeSong: 0,  // el tiempo actual de la canción
 			timerOn: null,// indica si el timer está prendido o no
 
-			// Cover Section Info
 			coverSectionData: {},
-
-			// Actors Section Info
 			actorsSectionData: {},
-
-			// Lyrics Section Info
 			lyricsSectionData: {},
-
-			// Associated Section Info
 			associatedSectionData: {},
-
-			// Highlighted Section Info
 			highlightedSectionData: {},
-
 			blackHoleSectionData: {},
-			
-			// las "cartas" de la canción seleccionada
 			cardsData: {},
 
 			// las estrofas de la canción seleccionada
@@ -49,23 +37,14 @@ class AnalysisPage extends React.Component {
 
 			// las "marcas de tiempo" de la canción seleccionada
 			timestamps: {}
-
 		}
   
 		this.getCurrentTimeSong = this.getCurrentTimeSong.bind(this);
 		this.getTimerInfo = this.getTimerInfo.bind(this);
-		this.getDataSong = this.getDataSong.bind(this);
    }
 
-	// Task: Ejecutar una vez que el componente se haya montado
 	componentDidMount() {
-
-		// Pone la ventana en el tope
 		window.scrollTo(0, 0)
-
-		// Mostrar un "activity view" hasta que lleguen los datos
-		console.log("Esperando los datos de la canción seleccionada")
-
 	}
 
 	// Task: Obtener el tiempo actual de la canción
