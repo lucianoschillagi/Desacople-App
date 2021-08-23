ceb2996bced60f3fda2de128c4dbaa07e469131e
Luciano <luko.web@gmail.com>
2021-08-23 19:07:52 -0300

    Removed unnecesary code

--- src/ui/pages/analysis/v.1.0/1.Another/another-one-bites-the-dust-cover.component.jsx
+++ src/ui/pages/analysis/v.1.0/1.Another/another-one-bites-the-dust-cover.component.jsx
@@ -1,80 +1,15 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import { Container,
-	ContainerCentered,
-	QuoteSong,
-	SongTitle,
-	Composers,
-	SongInfoContainer,
-	SongInfo,
-	FromYears
-} from '../../../../sections/cover/cover.styles.js'
+			ContainerCentered,
+			QuoteSong,
+			SongTitle,
+			Composers,
+			SongInfoContainer,
+			SongInfo,
+			FromYears
+		  } from '../../../../sections/cover/cover.styles.js'
 
-class Another_CoverSection extends Component {
-
-   // constructor(props) {
-	// 	super(props);
-	// 	this.state = { 
-	// 						// Loading...
-	// 						loading: true, // indica que la página se está cargando aún
-							
-	// 						// Song Timer
-	// 						currentTimeSong: 0,  // el tiempo actual de la canción
-	// 						timerOn: null,// indica si el timer está prendido o no
-
-	// 						// Cover Section Info
-	// 						coverSectionData: {},
-
-	// 						// Actors Section Info
-	// 						actorsSectionData: {},
-
-	// 						// Lyrics Section Info
-	// 						lyricsSectionData: {},
-
-	// 						// Associated Section Info
-	// 						associatedSectionData: {},
-
-	// 						// Highlighted Section Info
-	// 						highlightedSectionData: {},
-
-	// 						blackHoleSectionData: {},
-							
-	// 						// las "cartas" de la canción seleccionada
-	// 						cardsData: {},
-
-	// 						// las estrofas de la canción seleccionada
-	// 						estrofasArray: [],
-
-	// 						// las "marcas de tiempo" de la canción seleccionada
-	// 						timestamps: {}
-
-	// 				}
-  
-	// 	this.getCurrentTimeSong = this.getCurrentTimeSong.bind(this);
-	// 	this.getTimerInfo = this.getTimerInfo.bind(this);
-	// 	this.getDataSong = this.getDataSong.bind(this);
-   // }
-
-	// Task: Ejecutar una vez que el componente se haya montado
-	componentDidMount() {
-
-		window.scrollTo(0, 0); // Pone la ventana en el tope
-	}
-
-	
-
-	// Task: Obtener el tiempo actual de la canción
-  	// (dato generado en el componente PlaySong) ⏱
- 	getCurrentTimeSong(time) {
-		this.setState({ currentTimeSong: time })
-	}
-
-	// Task: Determinar si el timer está encendido
-	// obtener esa información
-	// (dato generado en el componente PlaySong)
-	getTimerInfo(timerState) {
-		this.setState({ timerOn: timerState })
-	}
+class Another_CoverSection extends React.Component {
 
 	render() {
 		return (
@@ -94,6 +29,7 @@ class Another_CoverSection extends Component {
 					</Composers>
 
 					<SongInfoContainer>
+
 						<SongInfo>
 								Album: The Game
 						</SongInfo>
