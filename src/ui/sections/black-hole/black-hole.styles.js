91b3a04e7606f4c232a36833f03f3a7cc729e96d
Luciano <luko.web@gmail.com>
2021-08-30 17:56:45 -0300

    Circle 'Black Hole' section bigger in desktop layout

--- src/ui/sections/black-hole/black-hole.styles.js
+++ src/ui/sections/black-hole/black-hole.styles.js
@@ -1,17 +1,16 @@
 import styled, { keyframes } from 'styled-components';
+import { device } from '../../../utils/devices-breakpoints';
 
 export const Container = styled.div`
 width: 100%;
 padding: 100px 0px 150px 0px;
 background-color: black;
 `
-
 const CircleAnimation = keyframes`
 from { transform: scale(0.9); }
 50% { transform: scale(1); }
 to { transform: scale(0.9); }
 `
-
 export const Circle = styled.div`
 margin: 0 auto;
 height: 240px;
@@ -24,5 +23,8 @@ animation-play-state: paused;
 /* 0.54s = 110 bpm */
 animation-duration: 0.54s; 
 animation-iteration-count: infinite;
+@media ${device.large} { 
+width: 500px;
+height: 500px;
+}
 `
-
