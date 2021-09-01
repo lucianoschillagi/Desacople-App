3b1aa51d84f979f9636b3e5d7647d87e629831d9
Luciano <luko.web@gmail.com>
2021-08-31 22:20:45 -0300

    Fixed text align in 'Content Of' component

--- src/ui/sections/listen/section-content.component.jsx
+++ src/ui/sections/listen/section-content.component.jsx
@@ -3,110 +3,109 @@ import './section-content.styles.css';
 
 class SectionContent extends React.Component { 
 
-    constructor(props) {
-        super(props);
-        this.state = { 
-                        sectionContent: this.sectionContentExample,
-                        prendido: false
-                     };
-    }
+   constructor(props) {
+		super(props);
+		this.state = { 
+							sectionContent: this.sectionContentExample,
+							prendido: false
+						 };
+	}
     
 	sectionContentExample = 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta).'
 
-    render() {
+   render() {
 
       //   convierte el valor de 'currentTimeSong' en un nro con un sólo decimal. Ej: 3.4
 		var currentTimeSongPrecision = Number(this.props.currentTimeSong.toFixed(1));
 
-        switch (currentTimeSongPrecision) {
-         case 0.5:
-        // Remove later: add 'prendido' property for avoid inifite loop!!!!
-        if (this.state.prendido === false) {
-            this.setState({ prendido: true, 
-							sectionContent: 'El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta.' })
-        }
-        break;
+		switch (currentTimeSongPrecision) {
+		case 0.5:
+		// Remove later: add 'prendido' property for avoid inifite loop!!!!
+		if (this.state.prendido === false) {
+			this.setState({ prendido: true, 
+						sectionContent: 'El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta.' })
+		}
+		break;
 
-        case 22.5:
-        if (this.state.prendido === true) {
-            this.setState({ prendido: false, 
-							sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
-        }
-        break;
+		case 22.5:
+		if (this.state.prendido === true) {
+			this.setState({ prendido: false, 
+						sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+		}
+		break;
 
-        case 40:
-        if (this.state.prendido === false) {
-            this.setState({ prendido: true, 
-							sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
-        }
-        break;
+		case 40:
+		if (this.state.prendido === false) {
+			this.setState({ prendido: true, 
+						sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+		}
+		break;
 
-        case 57.2:
-        if (this.state.prendido === true) {
-            this.setState({ prendido: false, 
-                            sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
-        }
-        break;
+		case 57.2:
+		if (this.state.prendido === true) {
+			this.setState({ prendido: false, 
+									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+		}
+		break;
 
-        case 66:
-        if (this.state.prendido === false) {
-            this.setState({ prendido: true, 
-                            sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
-        }
-        break;
-        
-        case 84:
-        if (this.state.prendido === true) {
-            this.setState({ prendido: false, 
-                            sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
-        }
-        break;
+		case 66:
+		if (this.state.prendido === false) {
+			this.setState({ prendido: true, 
+									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+		}
+		break;
+		
+		case 84:
+		if (this.state.prendido === true) {
+			this.setState({ prendido: false, 
+									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+		}
+		break;
 
-        case 101:
-        if (this.state.prendido === false) {
-            this.setState({ prendido: true, 
-                            sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
-        }
-        break;
-        case 140:
-        if (this.state.prendido === true) {
-            this.setState({ prendido: false, 
-                            sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
-        }
-        break;
+		case 101:
+		if (this.state.prendido === false) {
+			this.setState({ prendido: true, 
+									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+		}
+		break;
+		case 140:
+		if (this.state.prendido === true) {
+			this.setState({ prendido: false, 
+									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+		}
+		break;
 
-        case 158:
-        if (this.state.prendido === false) {
-            this.setState({ prendido: true, 
-                            sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
-        }
-        break;
+		case 158:
+		if (this.state.prendido === false) {
+			this.setState({ prendido: true, 
+									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+		}
+		break;
 
-        case 175:
-        if (this.state.prendido === true) {
-            this.setState({ prendido: false, 
-                            sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
-        }
-        break;
+		case 175:
+		if (this.state.prendido === true) {
+			this.setState({ prendido: false, 
+									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+		}
+		break;
 
-        case 193:
-        if (this.state.prendido === false) {
-            this.setState({ prendido: true, 
-                            sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
-        }
-        break;
+		case 193:
+		if (this.state.prendido === false) {
+			this.setState({ prendido: true, 
+									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+		}
+		break;
 
 
-        }
-
-        // Draw this UI elements...
-        return (
-            <div className="SectionContent_container">
-				<div className="SectionContent_content_of">
-					{this.state.sectionContent}
-				</div>
-            </div>
-        );
+		}
+		
+		return (
+			<div className="SectionContent_container">
+			<div className="SectionContent_content_of">
+				{this.state.sectionContent}
+			</div>
+			</div>
+		);
     }
 }
 
