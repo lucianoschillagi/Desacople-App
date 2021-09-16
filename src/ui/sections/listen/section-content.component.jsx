f5fe6aae29554a9da52082691b45984b7a8bd695
Luciano <luko.web@gmail.com>
2021-09-16 18:24:42 -0300

    Deployed first version ofr 'Section' content

--- src/ui/sections/listen/section-content.component.jsx
+++ src/ui/sections/listen/section-content.component.jsx
@@ -11,7 +11,7 @@ class SectionContent extends React.Component {
 						 };
 	}
     
-	sectionContentExample = 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta).'
+	sectionContentExample = 'La voz de MC acapella con delay da pie a la entrada de los drums y el bajo la voz repite 3 veces la misma frase y una frase de bajo da pie a verse'
 
    render() {
 
@@ -22,77 +22,70 @@ class SectionContent extends React.Component {
 		case 0.5:
 		// Remove later: add 'prendido' property for avoid inifite loop!!!!
 		if (this.state.prendido === false) {
-			this.setState({ prendido: true, 
-						sectionContent: 'El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta.' })
+			this.setState({ 
+						prendido: true, 
+						sectionContent: 'La voz de MC acapella con delay da pie a la entrada de los drums y el bajo la voz repite 3 veces la misma frase y una frase de bajo da pie a verse' })
 		}
 		break;
 
 		case 22.5:
 		if (this.state.prendido === true) {
-			this.setState({ prendido: false, 
-						sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+			this.setState({ 
+						prendido: false, 
+						sectionContent: 'a la voz principal y los drums y bajo se suma un stacatto de sinte que marca el tempo de reagge'})
 		}
 		break;
 
 		case 40:
 		if (this.state.prendido === false) {
-			this.setState({ prendido: true, 
-						sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+			this.setState({ 
+						prendido: true, 
+						sectionContent: 'la voz repite como un mantra la frase del chorus y el bajo desaparece y se suma un sonido agudo y sostenido en contraste con los graves de la base'})
 		}
 		break;
 
 		case 57.2:
 		if (this.state.prendido === true) {
-			this.setState({ prendido: false, 
-									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+			this.setState({ 
+						prendido: false, 
+						sectionContent: 'retoma los elementos del verse 1'})
 		}
 		break;
 
 		case 66:
 		if (this.state.prendido === false) {
-			this.setState({ prendido: true, 
-									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+			this.setState({ 
+						prendido: true, 
+						sectionContent: 'idem chorus 1'})
 		}
 		break;
 		
 		case 84:
 		if (this.state.prendido === true) {
-			this.setState({ prendido: false, 
-									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+			this.setState({ 
+						prendido: false, 
+						sectionContent: 'retoma el bajo con el motivo principal y se suman voces con delay'})
 		}
 		break;
 
 		case 101:
 		if (this.state.prendido === false) {
-			this.setState({ prendido: true, 
-									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+			this.setState({ 
+						prendido: true, 
+						sectionContent: 'la voz queda sobre los drums y desaparecen el sinte y el bajo para sumarse antes de dar lugar al chorus final'})
 		}
 		break;
 		case 140:
 		if (this.state.prendido === true) {
 			this.setState({ prendido: false, 
-									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
+									sectionContent: 'sobre el groove voces con delay suena el motivo de bajo y tras un silencio retoma el mismo modulando un tono más arriba'})
 		}
 		break;
 
 		case 158:
 		if (this.state.prendido === false) {
 			this.setState({ prendido: true, 
-									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
-		}
-		break;
-
-		case 175:
-		if (this.state.prendido === true) {
-			this.setState({ prendido: false, 
-									sectionContent: 'Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem.'})
-		}
-		break;
-
-		case 193:
-		if (this.state.prendido === false) {
-			this.setState({ prendido: true, 
-									sectionContent: 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,'})
+									sectionContent: 'retoma el verse pero en esta nueva tonalidad con una ligera variación en el fraseo de la voz'})
 		}
 		break;
 
